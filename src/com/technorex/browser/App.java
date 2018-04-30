ef29ab2e87ac10000def8a8ba689d3a1557238b7
Namanush <namanush.rsr.16@gmail.com>
2018-04-30 14:29:56 +0600

    Working on event handlers

--- src/com/technorex/browser/App.java
+++ src/com/technorex/browser/App.java
@@ -5,6 +5,7 @@ import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -18,7 +19,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * Main class that extends Application Class as to initiate the Turbo browser.
@@ -27,12 +30,14 @@ import javafx.stage.Stage;
  */
 
 public class App extends Application {
+
+    private boolean JSval = true;
+
     /**
      * Performs activity associated with the initialization of the Stage and Scene
      *
      * @param stage Input Stage.
      */
-    //private boolean JSVal;
     @Override
     public void start(Stage stage) {
         init(stage);
@@ -40,17 +45,33 @@ public class App extends Application {
     }
 
     private void init(Stage stage) {
+
+        /*
+          Necessary Variables
+         */
+        double scWidth = Screen.getPrimary().getBounds().getWidth();
+        double scHeight = Screen.getPrimary().getBounds().getHeight();
         final String DEFAULT_URL = "www.example.com";
         Group root = new  Group();
-        stage.setTitle("Turbo");
-        stage.getIcons().add(new Image("Icons/icon.png"));
-        stage.setScene(new Scene(root));
-        stage.setMaximized(true);
         BorderPane borderPane = new BorderPane();
         final TabPane tabPane = new TabPane();
-        tabPane.setPrefSize(1365.0,768.0);
-        tabPane.setSide(Side.TOP);
         final Tab newTab = new Tab();
+        Button goButton = new Button();
+        Button forward = new Button();
+        Button toggleJs = new Button();
+        Button backward = new Button();
+        ComboBox<String> history = new ComboBox<>();
+
+        /*
+          Logic and Graphics handling
+         */
+        stage.setTitle("Turbo");
+        stage.getIcons().add(new Image("Icons/icon.png"));
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setScene(new Scene(root,scWidth,scHeight));
+        stage.setMaximized(true);
+        tabPane.setPrefSize(scWidth,scHeight);
+        tabPane.setSide(Side.TOP);
         newTab.setText("+");
         newTab.setClosable(false);
         tabPane.getTabs().addAll(newTab);
@@ -63,39 +84,49 @@ public class App extends Application {
                 final WebEngine webEngine = webView.getEngine();
                 webEngine.load(DEFAULT_URL);
                 final TextField urlField = new TextField(DEFAULT_URL);
-                urlField.setMinHeight(35.0);
+                urlField.setMinHeight(36.0);
                 webEngine.locationProperty().addListener((observable1, oldValue, newValue) -> urlField.setText(newValue));
                 EventHandler<ActionEvent> goAction = event -> webEngine.load( (urlField.getText().startsWith("http://") ||urlField.getText().startsWith("https://"))
                         ? urlField.getText()
                         : "https://" + urlField.getText());
-                urlField.setOnAction(goAction);
-                Button goButton = new Button();
-                goButton.getStylesheets().add("/com/technorex/browser/GoButton.css");
-                goButton.setMinWidth(36.0);
-                goButton.setMinWidth(36.0);
+                EventHandler<ActionEvent> toggleJS = event -> {
+                    JSval = (!JSval);
+                    if(JSval) {
+                        webEngine.setJavaScriptEnabled(JSval);
+                        toggleJs.getStylesheets().add("/com/technorex/browser/ToggleJs.css");
+                        webEngine.reload();
+                    }
+                    else {
+                        webEngine.setJavaScriptEnabled(JSval);
+                        toggleJs.getStylesheets().add("/com/technorex/browser/notToggleJs.css");
+                        webEngine.reload();
+                    }
+                };
+
+                /*
+                Defining button sizes and styles
+                 */
+                goButton.setPrefSize(36.0,36.0);
+                toggleJs.setPrefSize(36.0,36.0);
+                forward.setPrefSize(36.0,36.0);
+                backward.setPrefSize(36.0,36.0);
+                history.setMaxSize(36.0,36.0);
                 goButton.setDefaultButton(true);
-                goButton.setOnAction(goAction);
-                Button toggleJs = new Button();
-                toggleJs.setMinWidth(36.0);
-                toggleJs.setMinWidth(36.0);
-                toggleJs.getStylesheets().add("/com/technorex/browser/ToggleJs.css");
                 toggleJs.setDefaultButton(true);
-                Button forward = new Button();
-                forward.setMinWidth(36.0);
-                forward.setMinWidth(36.0);
-                forward.getStylesheets().add("/com/technorex/browser/Forward.css");
                 forward.setDefaultButton(true);
-                Button backward = new Button();
-                backward.setMinWidth(36.0);
-                backward.setMinWidth(36.0);
-                backward.getStylesheets().add("/com/technorex/browser/Backward.css");
                 backward.setDefaultButton(true);
-                ComboBox<String> history = new ComboBox<>();
-                history.setMinWidth(36.0);
-                history.setMinWidth(36.0);
+                goButton.getStylesheets().add("/com/technorex/browser/GoButton.css");
+                toggleJs.getStylesheets().add("/com/technorex/browser/ToggleJs.css");
+                forward.getStylesheets().add("/com/technorex/browser/Forward.css");
+                backward.getStylesheets().add("/com/technorex/browser/Backward.css");
                 history.getStylesheets().add("/com/technorex/browser/HistoryButton.css");
-                //history.setDefaultButton(true);
-                //toggleJs.setOnAction(toggleJS());
+
+                /*
+                Adding event handlers to buttons
+                 */
+                urlField.setOnAction(goAction);
+                goButton.setOnAction(goAction);
+                toggleJs.setOnAction(toggleJS);
                 HBox hBox = new HBox(10);
                 hBox.getChildren().setAll(history,backward,forward,toggleJs,urlField, goButton);
                 hBox.setStyle("-fx-background-color: #f7f7f7");
@@ -119,27 +150,19 @@ public class App extends Application {
     private void createAndSelectNewTab(final TabPane tabPane) {
         Tab tab = new Tab("Home");
         Label aboutLabel = new Label();
-        aboutLabel.setText("\n\n\n\n\t\t\t\tTURBO" +
-                "\n\n\t\tA lightweight, Secure and User Friendly Web Browser" +
-                "\n\t\tCSE-2112 Java project, CSEDU" +
-                "\n\t\tStart browsing by opening new tab");
+        aboutLabel.setText("\n\n\n\n\t\t\t\t\t\t\tTURBO" +
+                "\n\n\t\t\t\t\tA lightweight, Secure and User Friendly Web Browser" +
+                "\n\t\t\t\t\tCSE-2112 Java project, CSEDU" +
+                "\n\t\t\t\t\tStart browsing by opening new tab");
         aboutLabel.setFont(Font.font("Consolas", FontWeight.BOLD, 20));
+        aboutLabel.setAlignment(Pos.TOP_CENTER);
         tab.setContent(aboutLabel);
         final ObservableList<Tab> tabs = tabPane.getTabs();
         tab.closableProperty().bind(Bindings.size(tabs).greaterThan(2));
         tabs.add(tabs.size() - 1, tab);
         tabPane.getSelectionModel().select(tab);
     }
-    public void toggleJS() {
-        /*if (JSVal) {
-            //webEngine.setJavaScriptEnabled(false);
-        }
-        else {
-            //webEngine.setJavaScriptEnabled(true);
-        }
-        JSVal = !JSVal; //Toggles the value of JSVal every time this function is called
-        */
-    }
+
     public static void main(String[] args) {
         launch(args);
     }
