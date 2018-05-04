53ae92a1301e43692b5bdde340b2fce3ade75298
sihan-tawsik <sihantawsik@gmail.com>
2018-05-04 16:11:13 +0600

    Added Download Class.

---
+++ src/com/technorex/browser/DownloadThread.java
@@ -0,0 +1,94 @@
+package com.technorex.browser;
+
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * A utility that downloads a file from a URL.
+ * @author Sihan Tawsik, Samnan Rahee
+ *
+ */
+class DownloadThread extends Thread {
+    private static final int BUFFER_SIZE = 4096;
+    private String fileURL;
+    private Stage primaryStage;
+    DownloadThread(String name,String fileURL,Stage primaryStage){
+        super(name);
+        this.fileURL=fileURL;
+        this.primaryStage=primaryStage;
+    }
+    @Override
+    public void run() {
+        try {
+            fleDirectory();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fleDirectory() throws IOException {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("JavaFX Projects");
+        File defaultDirectory = new File("c:/dev/javafx");
+        chooser.setInitialDirectory(defaultDirectory);
+        downloadFile(chooser.showDialog(primaryStage).getPath());
+    }
+    private void downloadFile(String saveDir) throws IOException {
+        URL url = new URL(fileURL);
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        int responseCode = httpConn.getResponseCode();
+
+        // always check HTTP response code first
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            String fileName = "";
+            String disposition = httpConn.getHeaderField("Content-Disposition");
+            String contentType = httpConn.getContentType();
+            int contentLength = httpConn.getContentLength();
+
+            if (disposition != null) {
+                // extracts file name from header field
+                int index = disposition.indexOf("filename=");
+                if (index > 0) {
+                    fileName = disposition.substring(index + 10,
+                            disposition.length() - 1);
+                }
+            } else {
+                // extracts file name from URL
+                fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
+                        fileURL.length());
+            }
+
+            System.out.println("Content-Type = " + contentType);
+            System.out.println("Content-Disposition = " + disposition);
+            System.out.println("Content-Length = " + contentLength);
+            System.out.println("fileName = " + fileName);
+
+            // opens input stream from the HTTP connection
+            InputStream inputStream = httpConn.getInputStream();
+            String saveFilePath = saveDir + File.separator + fileName;
+
+            // opens an output stream to save into file
+            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+            int bytesRead;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+            System.out.println("File downloaded");
+        } else {
+            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+        }
+        httpConn.disconnect();
+    }
+}
